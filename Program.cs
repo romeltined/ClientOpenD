a6826d69fc4e5fd7d2e57cbc0f25f8fc855926f2
Romelito Tined <rstined@yahoo.com>
2020-09-01 18:48:14 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Default;
+using HelloOpenData.Models;
+
+namespace ClientOpenD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListReader().Wait();
+        }
+
+        static async Task ListReader()
+        {
+            var serviceRoute = @"https://localhost:44398/odata";
+            var context = new Container(new Uri(serviceRoute));
+
+            IEnumerable<Reader> reader = await context.AllReaders().ExecuteAsync();
+            
+                
+
+            foreach(var r in reader)
+            {
+                Console.WriteLine("{0}, {1}", r.UserName, r.IsReaderActive);
+            }
+
+        }
+
+ 
+    }
+
+}
